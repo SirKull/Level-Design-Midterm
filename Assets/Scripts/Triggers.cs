6336051e9e6dd6cdaccad04c6bf258b27f2963dd
SirKull <adamkull@lcad.edu>
2023-10-20 16:46:47 -0700

    Latest Version

--- Assets/Scripts/Triggers.cs
+++ Assets/Scripts/Triggers.cs
@@ -12,18 +12,34 @@ public class Triggers : MonoBehaviour
     public float gateSpeed = .3f;
     public Vector3 endPosition = new Vector3(0f, 3f, 0f);
     public bool _gateTriggered = false;
-  
+    public bool _gateTriggeredFirst = false;
+
 
     [Header("Main Power")]
+    public Transform mainPower;
     public bool _mainPowerOn = false;
     public bool _powerTriggered = false;
     public Transform powerTrigger;
-    
+
+    [Header("Lights")]
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    public GameObject light4;
+
+    [Header("Doors")]
+    public GameObject door1;
+    public GameObject door2;
+
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = gate.transform.position;
+        light1.SetActive(false);
+        light2.SetActive(false);
+        light3.SetActive(false);
+        light4.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,7 +51,12 @@ public class Triggers : MonoBehaviour
             {
                 var step = gateSpeed * Time.deltaTime;
                 gate.transform.position = Vector3.MoveTowards(initialPosition, endPosition, step);
+                DestroyDoors();
             }
+        }
+        if (_gateTriggeredFirst == true)
+        {
+            EnableLights();
         }
        
     }
@@ -45,13 +66,32 @@ public class Triggers : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider collision)
+    private void GateTrigger(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            _gateTriggeredFirst = true;
             _gateTriggered = true;
         }
     }
 
-    
+    private void DestroyDoors()
+    {
+        if (_mainPowerOn == true)
+        {
+            Destroy(door1);
+            Destroy(door2);
+        }
+    }
+
+    private void EnableLights()
+    {
+        if(_gateTriggeredFirst == true)
+        {
+            light1.SetActive(false);
+            light2.SetActive(false);
+            light3.SetActive(false);
+            light4.SetActive(false);
+        }
+    }
 }
